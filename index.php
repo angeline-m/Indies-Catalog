6c9de1ac206ae8fd8245c5da0f30663204e89c51
unknown <shelite17@gmail.com>
2021-10-28 23:35:22 -0600

    Updated index

--- index.php
+++ index.php
@@ -22,16 +22,16 @@ $random = mysqli_query($con, "SELECT * FROM ama_games ORDER BY RAND() LIMIT 1") 
     <p class="w-75 mx-auto">Most of these games are friendly for new gamers, young gamers, and even people that don't play games! I can guarantee that everyone will find a game in this catalog that they'd like, so come in and check it out!</b></p>
     <p class="w-75 mx-auto"><b>To create, update, or delete game entries, please login.</b> Login credentials in the <a href="https://github.com/angeline-m/indies-catalog" target="_blank">Github Repository</a>.</p>
     <p class="w-75 mx-auto">This catalog was made using PHP, MySQL, and Bootstrap 4.</p>
-    <div class="mt-3 mx-auto d-inline-flex flex-wrap">
-        <a href="display.php?id=<?php echo $gameID; ?>" class="mx-2">
+    <div class="mt-3 mx-auto">
+        <!-- <a href="display.php?id=<?php echo $gameID; ?>" class="mx-2">
             <button type="button" class="btn btn-secondary bg-warning yellow mb-2">I'm feeling lucky!</button>
-        </a>
+        </a> -->
         <a href="main.php" class="mx-2">
-            <button type="button" class="btn btn-secondary grey mb-2">Lemme see all the games!</button>
+            <button type="button" class="btn btn-secondary grey mb-2">See the games</button>
         </a>
-        <a href="main.php?displayby=ama_price&max=10" class="mx-2">
+        <!-- <a href="main.php?displayby=ama_price&max=10" class="mx-2">
             <button type="button" class="btn btn-secondary bg-info blue mb-2">Show the cheap games first!</button>
-        </a>
+        </a> -->
     </div>
 </div>
 
